9eb4d8009224cf19c18bc694220c5399c5c1ca3b
Travis Gregory <tgregory@shutterstock.com>
2024-12-02 14:33:34 -0500

    add localStorage functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,33 @@
+// Initialize storage functions
+const storage = {
+  save: function(bookmarks) {
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  },
+  load: function() {
+    const bookmarks = localStorage.getItem('bookmarks');
+    return bookmarks ? JSON.parse(bookmarks) : [];
+  },
+  getBookmarks: function() {
+    return $('.bookmark').map(function() {
+      const $bookmark = $(this);
+      return {
+        title: $bookmark.find('h2').text(),
+        url: $bookmark.find('a').attr('href'),
+        isRead: $bookmark.hasClass('read-count')
+      };
+    }).get();
+  }
+};
+
+// Load saved bookmarks on page load
+$(document).ready(function() {
+  const savedBookmarks = storage.load();
+  savedBookmarks.forEach(function(bookmark) {
+    makeBookmark(bookmark.title, bookmark.url, bookmark.isRead);
+  });
+  counter();
+});
+
 //disable enter button on startup
 $('#enter').prop('disabled', true);
 
@@ -17,7 +47,7 @@ $('#enter').on('click', function(event) {
   event.preventDefault();
   var $name = $('#title').val();
   var $url = $('#address').val();
-  checkForText ($name, $url);
+  checkForText($name, $url);
   counter();
   $('#enter').prop('disabled', true);
 })
@@ -37,34 +67,35 @@ function checkForAddress($name, $url) {
   if(check.test($url) === false) {
     alert('Please enter a valid URL that includes http://, https://, or ftp:// as a prefix.');
   } else {
-    makeBookmark($name, $url);
+    makeBookmark($name, $url, false);
     $('#title').val('');
     $('#address').val('');
   }
 }
 
 //creates HTML and enters information into object
-function makeBookmark($name, $url) {
-  //create bookmark
+function makeBookmark($name, $url, isRead = false) {
   const bookmarkTemplate = `
-    <div class="bookmark">
+    <div class="bookmark${isRead ? ' read-count' : ''}">
       <h2>${$name}</h2>
       <div class="break"></div>
       <a href="${$url}" 
-         class="original-underline text-regular">${$url}</a>
+         class="original-underline text-regular${isRead ? ' activated' : ''}">${$url}</a>
       <div class="break"></div>
-      <button class="original-underline read">Read</button>
+      <button class="original-underline read${isRead ? ' read-button-toggled' : ''}">Read</button>
       <button class="original-underline delete">Delete</button>
     </div>
   `;
 
   $('.bookmarks-list').prepend(bookmarkTemplate);
-};
+  storage.save(storage.getBookmarks());
+}
 
 //delete button
 $('.bookmarks-list').on('click','.delete', function() {
   $(this).parent('.bookmark').remove();
   counter();
+  storage.save(storage.getBookmarks());
 });
 
 //read button and toggle of read status
@@ -74,6 +105,7 @@ $('.bookmarks-list').on('click','.read', function() {
   $bookmark.find('a').toggleClass('activated');
   $(this).toggleClass('read-button-toggled');
   counter();
+  storage.save(storage.getBookmarks());
 });
 
 //delete button for read links
@@ -81,6 +113,7 @@ $('#delete-read').on('click', function(event) {
   event.preventDefault();
   $('.read-count').remove();
   counter();
+  storage.save(storage.getBookmarks());
 });
 
 //counter for number of entries, read, and unread
@@ -92,7 +125,7 @@ function counter () {
   $('#total-read').text(readCount);
   $('#unread').text(unreadCount);
   leftHeight(totalCount);
-};
+}
 
 //detect window resize
 $(window).on('resize', function(){
